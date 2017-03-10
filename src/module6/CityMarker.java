ac4ffb64e3961a454edcc5d2b3b8e67093a16637
An Le <an.le@sv.cmu.edu>
2017-03-10 03:30:08 -0800

    Init
    Init

---
+++ src/module6/CityMarker.java
@@ -0,0 +1,123 @@
+package module6;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.geo.Location;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+/** Implements a visual marker for cities on an earthquake map
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * 
+ */
+public class CityMarker extends CommonMarker {
+	private float totalMag = 0;
+	private int quakeCount = 0;
+	public static int TRI_SIZE = 5;  // The size of the triangle marker
+	
+	public CityMarker(Location location) {
+		super(location);
+	}
+	
+	
+	public CityMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+		// Cities have properties: "name" (city name), "country" (country name)
+		// and "population" (population, in millions)
+	}
+	
+	
+	// pg is the graphics object on which you call the graphics
+	// methods.  e.g. pg.fill(255, 0, 0) will set the color to red
+	// x and y are the center of the object to draw. 
+	// They will be used to calculate the coordinates to pass
+	// into any shape drawing methods.  
+	// e.g. pg.rect(x, y, 10, 10) will draw a 10x10 square
+	// whose upper left corner is at position x, y
+	/**
+	 * Implementation of method to draw marker on the map.
+	 */
+	public void drawMarker(PGraphics pg, float x, float y) {
+		//System.out.println("Drawing a city");
+		// Save previous drawing style
+		pg.pushStyle();
+		
+		// IMPLEMENT: drawing triangle for each city
+		pg.fill(150, 30, 30);
+		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
+		
+		// Restore previous drawing style
+		pg.popStyle();
+	}
+	
+	/** Show the title of the city if this marker is selected */
+	public void showTitle(PGraphics pg, float x, float y)
+	{
+		String name = getCity() + " " + getCountry() + " ";
+		String pop = "Pop: " + getPopulation() + " Million";
+		
+		pg.pushStyle();
+		
+		pg.fill(255, 255, 255);
+		pg.textSize(12);
+		pg.rectMode(PConstants.CORNER);
+		pg.rect(x, y-TRI_SIZE-39, Math.max(pg.textWidth(name), pg.textWidth(pop)) + 6, 39);
+		pg.fill(0, 0, 0);
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.text(name, x+3, y-TRI_SIZE-33);
+		pg.text(pop, x+3, y - TRI_SIZE -18);
+		
+		pg.popStyle();
+	}
+	
+	public void showDetails(PGraphics pg, float x, float y){
+		String quakeCounts = getQuakeCount() + " ";
+		String average = "Average: " + (getTotalMag()/getQuakeCount()) + " ";
+		
+		pg.pushStyle();
+		
+		pg.fill(255, 255, 255);
+		pg.textSize(12);
+		pg.rectMode(PConstants.CORNER);
+		pg.rect(x, y-TRI_SIZE-39, Math.max(pg.textWidth(quakeCounts), pg.textWidth(average)) + 6, 39);
+		pg.fill(0, 0, 0);
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.text(quakeCounts, x+3, y-TRI_SIZE-33);
+		pg.text(average, x+3, y - TRI_SIZE -18);
+		
+		pg.popStyle();
+	}
+	
+	
+	private String getCity()
+	{
+		return getStringProperty("name");
+	}
+	
+	private String getCountry()
+	{
+		return getStringProperty("country");
+	}
+	
+	private float getPopulation()
+	{
+		return Float.parseFloat(getStringProperty("population"));
+	}
+	
+	private int getQuakeCount(){
+		return quakeCount;
+	}
+	
+	private float getTotalMag(){
+		return totalMag;
+	}
+	
+	public void setTotalMag(float mag){
+		this.totalMag = mag;
+	}
+	
+	public void setQuakeCount(int count){
+		quakeCount = count;
+	}
+}
